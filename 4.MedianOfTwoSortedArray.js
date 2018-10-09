7566f44f13bf7a1e5d1f09f7bbac2c810b4ab18d
pnn2104@columbia.edu <pnn2104@columbia.edu>
2018-10-09 17:06:42 -0400

    improved time complexity for findMedianOfTwoSortedArrays

--- 4.MedianOfTwoSortedArray.js
+++ 4.MedianOfTwoSortedArray.js
@@ -1,3 +1,40 @@
+var findMedianOfArrayWithBinarySearch = function(nums1, nums2) {
+    if (nums1.length < nums2.length) {
+        var x = nums1;
+        var y = nums2;
+    } else {
+        x = nums2;
+        y = nums1;
+    }
+
+    var startX = 0;
+    var endX = x.length - 1;
+    
+    while (startX <= endX) {
+        var midX = Math.floor((startX + endX) / 2);
+        var midY = Math.floor(((x.length + y.length + 1) / 2)) - midX;
+        var x_left = x[midX - 1] ? x[midX - 1] : -Infinity;
+        var y_left = y[midY - 1] ? y[midY - 1] : -Infinity;
+        var x_right = x[midX] ? x[midX] : Infinity;
+        var y_right = y[midY] ? y[midY] : Infinity;
+        if (x_left <= y_right && y_left <= x_right) {
+            if ((x.length + y.length) % 2 === 0) {
+                //console.log(x_left, y_left, x_right, y_right);
+                return (Math.max(x_left, y_left) + Math.min(x_right, y_right)) / 2
+            } else {
+                console.log("here: ", x.left, y_left)
+                return Math.max(x_left, y_left);
+            }
+        } else if (x_left > y_right) {
+            endX = midX - 1;
+        } else if (y_right > x_right){
+            startX = midX + 1;
+        }
+    }
+    
+}
+
+
 var findMedianSortedArrays = function(nums1, nums2) {
     var arr = nums1.concat(nums2).sort((a, b) => { //O(nlogn)
         return a - b;
@@ -17,7 +54,32 @@ var findMedianOfArray = function(arr) {
     }
 }
 
+var nums1 = [1, 3];
+var nums2 = [2];
+//var nums1 = [23, 26, 31, 33];
+//var nums2 = [3, 5, 7, 9, 11, 16];
+/*
+23                26, 31, 33
+3, 5, 7, 9           11, 16
+*/
+console.log(findMedianOfArrayWithBinarySearch(nums1, nums2));
+/*
+[1, 3, 7, 8]  ==> max = 8
+[3, 4, 5] ==> min = 3
 
-var nums1 = [3];
-var nums2 = [-2, -1];
+1, 3, 4, 5, 7, 8
 
+Left                    Right
+[1, 3, 7]               [8]
+[3, 4]                  [5]
+
+5 elements             2 elements
+Max Left = 7           Min Right = 5
+
+Left                    Right
+[1, 3]                  [8]  7
+[ 4]                  [5]
+Max Left = 4            Min Right = 5
+Median (4 + 5) /2 
+
+*/

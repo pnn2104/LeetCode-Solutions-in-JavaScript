b894f4b94b10e9b96108ebd4a430317492e47516
pnn2104@columbia.edu <pnn2104@columbia.edu>
2018-10-10 23:35:53 -0400

    median of two sorted array

--- 4.MedianOfTwoSortedArray.js
+++ 4.MedianOfTwoSortedArray.js
@@ -8,7 +8,7 @@ var findMedianOfArrayWithBinarySearch = function(nums1, nums2) {
     }
 
     var startX = 0;
-    var endX = x.length - 1;
+    var endX = x.length;
     
     while (startX <= endX) {
         var midX = Math.floor((startX + endX) / 2);
@@ -17,24 +17,26 @@ var findMedianOfArrayWithBinarySearch = function(nums1, nums2) {
         var y_left = y[midY - 1] ? y[midY - 1] : -Infinity;
         var x_right = x[midX] ? x[midX] : Infinity;
         var y_right = y[midY] ? y[midY] : Infinity;
+        console.log("hereee: ", x_left, x_right, y_left, y_right)
+        
         if (x_left <= y_right && y_left <= x_right) {
+            
             if ((x.length + y.length) % 2 === 0) {
                 //console.log(x_left, y_left, x_right, y_right);
                 return (Math.max(x_left, y_left) + Math.min(x_right, y_right)) / 2
             } else {
-                console.log("here: ", x.left, y_left)
                 return Math.max(x_left, y_left);
             }
         } else if (x_left > y_right) {
             endX = midX - 1;
-        } else if (y_right > x_right){
+        } else {
             startX = midX + 1;
         }
     }
     
 }
 
-
+/*
 var findMedianSortedArrays = function(nums1, nums2) {
     var arr = nums1.concat(nums2).sort((a, b) => { //O(nlogn)
         return a - b;
@@ -52,17 +54,18 @@ var findMedianOfArray = function(arr) {
         var right = Math.ceil((arr.length - 1) / 2);
         return (arr[left] + arr[right])/2
     }
-}
+}*/
 
-var nums1 = [1, 3];
-var nums2 = [2];
-//var nums1 = [23, 26, 31, 33];
-//var nums2 = [3, 5, 7, 9, 11, 16];
+//var nums1 = [1, 3];
+//var nums2 = [2];
+var nums1 = [23, 26, 31, 33];
+var nums2 = [3, 5, 7, 9, 11, 16];
 /*
 23                26, 31, 33
 3, 5, 7, 9           11, 16
 */
 console.log(findMedianOfArrayWithBinarySearch(nums1, nums2));
+
 /*
 [1, 3, 7, 8]  ==> max = 8
 [3, 4, 5] ==> min = 3

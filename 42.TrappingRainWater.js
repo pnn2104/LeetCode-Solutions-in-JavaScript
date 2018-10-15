f486ae464d2afca3cdcc887c4e1c209aa99fc502
pnn2104@columbia.edu <pnn2104@columbia.edu>
2018-10-14 22:50:07 -0400

    trapping rain water dp approach

--- 42.TrappingRainWater.js
+++ 42.TrappingRainWater.js
@@ -1,3 +1,4 @@
+//brute force O(n^2);
 var trap = function(arr) {
     var maxLeft = 0;
     var maxRight = Math.max(...arr.slice(1));
@@ -15,6 +16,30 @@ var trap = function(arr) {
     return amountOfWater;
 }
 
+var trapDP = function(arr) {
+    var maxLeft = 0;
+    var maxRight = 0;
+    var dp = new Array(arr.length);
+    //var fromRight = new Array(arr.length);
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > maxLeft) {
+            maxLeft = arr[i];
+        }
+        dp[i] = maxLeft - arr[i];   
+    }
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] > maxRight) {
+            maxRight = arr[i];
+        }
+        dp[i] = Math.min(dp[i], maxRight - arr[i]);
+    }
+
+    return dp.reduce(function(num, acc) {
+        return num += acc;
+    }, 0)
+}
+
 var test = [0,1,0,2,1,0,1,3,2,1,2,1];
 
-console.log(trap(test));
+console.log(trapDP(test));

33df7f13c4ee7dfbc7b55be7fab142b27e143b40
pnn2104@columbia.edu <pnn2104@columbia.edu>
2018-10-22 14:46:16 -0400

    dp approach for knight probability

--- 688.KnightProbabilityInChessboard.js
+++ 688.KnightProbabilityInChessboard.js
@@ -1,10 +1,43 @@
 // N is size of chessboard,
 // K is number of moves it can make,
 // r, c start row and column
-var knightProbability = function(N, K, r, c) {
+
+//DP approach
+var knightProbability= function(N, K, r, c) {
+    var dp = [...new Array(N)].map(() => new Array(N).fill(0));
+    dp[r][c] = 1;
+    var allMoves = [[-2, -1], [-1, -2], [-2, 1], [-1, 2], [2, -1], [1, -2], [2, 1], [1, 2]];
+
+    for (let numOfMove = 0; numOfMove < K; numOfMove++) {
+        var dp1 = [...new Array(N)].map(() => new Array(N).fill(0));
+        for (let row = 0; row < dp.length; row++) {
+            for (let col = 0; col < dp[row].length; col++) {
+                for (let move of allMoves) {
+                    if (row + move[0] >= 0 && row + move[0] <= N - 1 && col + move[1] >= 0 && col + move[1] <= N - 1) {
+                        dp1[row + move[0]][col + move[1]] += dp[row][col] / 8;
+                    }
+                }
+                
+            }
+        }
+        dp = dp1;
+    }
+    var result = dp.reduce(function(row, acc) {
+        return acc = acc.concat(row)
+    }, []).reduce(function(num, acc) {
+        return acc += num
+    })
+
+    return result;
+}
+
+//DFS with memoization
+var knightProbabilityDFS = function(N, K, r, c) {
     var memo = {};
     var numOfMoves = dfs(N, K, r, c, memo);
     var allpossibilities = Math.pow(8, K);
+    
+    var queue
     
     return numOfMoves / allpossibilities;
 };
@@ -44,4 +77,6 @@ function dfs(N, K, r, c, memo) {
 
 
 
-console.log(knightProbability(12, 15, 8, 0 ));
+
+
+console.log(knightProbability(3, 2, 0, 0 ));

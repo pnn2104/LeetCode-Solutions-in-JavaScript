a5af654966e9f82ef561785a5925550e340543c6
pnn2104@columbia.edu <pnn2104@columbia.edu>
2018-11-01 16:31:44 -0400

    revised

--- 121.BestTimeToBuyAndSellStock.js
+++ 121.BestTimeToBuyAndSellStock.js
@@ -2,14 +2,9 @@ var maxProfit = function(prices) {
     var minPrice = prices[0];
     var maxProfit = 0;
     for (var i = 1; i < prices.length; i++) {
-        if (prices[i] < minPrice) {
-            minPrice = prices[i];
-        } else {
-            var newProfit = prices[i] - minPrice;
-            if (newProfit > maxProfit) {
-                maxProfit = newProfit;
-            }
-        }
+        var newProfit = prices[i] - minPrice;
+        maxProfit = Math.max(maxProfit, newProfit);
+        minPrice = Math.min(minPrice, prices[i]); 
     }
     return maxProfit;
 };

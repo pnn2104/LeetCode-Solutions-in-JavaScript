29bb30647a0c28fdae739802fddcf8072157654a
pnn2104@columbia.edu <pnn2104@columbia.edu>
2018-11-24 17:29:30 -0500

    palindrome permutation

---
+++ 266.PalindromePermutation.js
@@ -0,0 +1,12 @@
+var canPermutePalindrome = function(s) {
+    let set = new Set ();
+    for (let char of s) {
+        if (set.has(char)) {
+            set.delete(char);
+        } else {
+            set.add(char);
+            
+        }
+    }
+    return set.size <= 1;
+}
